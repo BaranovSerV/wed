6a12db8fbdf2ecaed3f35d0f89c98ab5b96304a0
Sergey Baranov <167512661+BaranovSerV@users.noreply.github.com>
2024-08-15 20:32:05 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const section = document.getElementById('dresscode');
+
+    function isElementInViewport(el) {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top < (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.bottom >= 0
+        );
+    }
+
+    function handleScroll() {
+        if (isElementInViewport(section)) {
+            section.classList.add('animate');
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll(); // Запуск функции для проверки на момент загрузки страницы
+});
 
